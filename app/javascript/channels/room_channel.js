c3bb5a64eef3c52964a90e0cf5fe5767914fec7e
noah <noahtamulonis@gmail.com>
2021-07-13 14:38:23 -0400

    broken :/

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,16 +1,39 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({channel: "RoomChannel", room_id: 2), {
-  connected() {
-    console.log("connected...")
-    // Called when the subscription is ready for use on the server
-  },
+// document.addEventListener('turbolinks:load', () => {
+//  const room_element = document.getElementById('room-id');
+//  const room_id = Number(room_element.getElementById('data-room-id');
+//
+//  console.log(consumer.subscriptions)
+//
+//    consumer.subscription.subscriptions.forEach((subscription) => {
+//      consumer.subscriptions.remove(subscription)
+//    })
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
 
-  received(data) {
-    console.log(data)
-  }
-});
+//    consumer.subscriptions.create({channel: "RoomChannel", room_id: room_id }, {
+//      connected() {
+//        // Called when the subscription is ready for use on the server
+//      },
+//
+//      disconnected() {
+//        // Called when the subscription has been terminated by the server
+//      },
+//
+//      received(data) {
+//        const user_element = document.getElementById('user-id');
+//        const user_id = Number(user_element.getElementById('data-user-id'));
+//
+//        let html;
+
+//        if (user_id === data.message.user_id) {
+//          html = data.mine
+//        } else {
+//        html = data.theirs
+//        }
+
+//        const messageContainer = document.getElementById('messages')
+//        messageContainer.innerHTML = messageContainer.innerHTML + data.html
+//      }
+//    });
+//})
