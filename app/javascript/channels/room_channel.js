93a78923bbeccc9caeebcf4fb033bb199836112e
noah <noahtamulonis@gmail.com>
2021-07-14 12:41:48 -0400

    actioncable

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,39 +1,39 @@
 import consumer from "./consumer"
 
-// document.addEventListener('turbolinks:load', () => {
-//  const room_element = document.getElementById('room-id');
-//  const room_id = Number(room_element.getElementById('data-room-id');
-//
-//  console.log(consumer.subscriptions)
-//
-//    consumer.subscription.subscriptions.forEach((subscription) => {
-//      consumer.subscriptions.remove(subscription)
-//    })
+document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = room_element.getAttribute('data-room-id');
 
+  console.log(consumer.subscriptions)
 
-//    consumer.subscriptions.create({channel: "RoomChannel", room_id: room_id }, {
-//      connected() {
-//        // Called when the subscription is ready for use on the server
-//      },
-//
-//      disconnected() {
-//        // Called when the subscription has been terminated by the server
-//      },
-//
-//      received(data) {
-//        const user_element = document.getElementById('user-id');
-//        const user_id = Number(user_element.getElementById('data-user-id'));
-//
-//        let html;
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    consumer.subscriptions.remove(subscription)
+  })
 
-//        if (user_id === data.message.user_id) {
-//          html = data.mine
-//        } else {
-//        html = data.theirs
-//        }
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
+    connected() {
+      console.log("connected to " + room_id)
+      // Called when the subscription is ready for use on the server
+    },
 
-//        const messageContainer = document.getElementById('messages')
-//        messageContainer.innerHTML = messageContainer.innerHTML + data.html
-//      }
-//    });
-//})
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      const user_element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+
+      let html;
+
+      if (user_id === data.message.user_id) {
+        html = data.mine
+      } else {
+        html = data.theirs
+      }
+
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
+    }
+  });
+})
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,12 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :username
 
-def self.generate
-  adjectives = ['Ancent', 'Broken', 'Creative', 'dangerous', 'Effective', 'Flying', 'Gilded']
-  nouns = ['highway', 'Intern', 'Fish', 'Nurse', 'Bone']
-  number = rand.to_s[2..4]
-  username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
-  create(username: username)
-end
-
+  def self.generate
+    adjectives = ['Ancient', 'Creative', 'Dangerous', 'Effective', 'Flying', 'Gilded']
+    nouns = ['Highway', 'Intern', 'Jackhammer', 'Lion', 'Master']
+    number = rand.to_s[2..4]
+    username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+    create(username: username)
+  end
 
 end
